c4942d401057cbc061611bcf219f7942b5c087c4
Yen <49903454+Yen13Sky@users.noreply.github.com>
2021-07-02 13:24:26 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Test</title>
+</head>
+<body>
+<div class="wrapper">
+    <div class="container">
+        <div class="main-page">
+            <header class="header">
+                <div class="main-page-title">
+                    <h1>Вывод данных  из БД. Таблица test</h1>
+                </div>
+                <div class="menu-list">
+                <div class="header-btn">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                    <ul class="list">
+                        <li>Информация</li>
+                        <li>Помощь</li>
+                        <li>Добавить запись</li>
+                    </ul>
+                </div>
+            </header>
+
+            <form class="table">
+                <table class="table-data">
+                </table>
+                <button type="submit" class="form-btn">Показать данные таблицы tasks</button>
+            </form>
+        </div>
+    </div>
+</div>
+<script src="js/script.js"></script>
+</body>
+</html>
